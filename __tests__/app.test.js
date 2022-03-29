d401796b5a9e825af3d34ae9c8eae0a505daf6de
joshua360x <jkw5499@psu.edu>
2022-03-29 17:31:42 -0400

    added post route to sign user in

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,16 @@
- const pool = require('../lib/utils/pool');
+const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-
+const UserService = require('../lib/services/UserService');
+const agent = request.agent(app);
 
 //create user for testing
 const userToBeTested = {
   email: 'test@test.com',
   password: 'bingo'
 };
+
 
 describe('alchemy-app routes', () => {
   beforeEach(() => {
@@ -26,6 +28,17 @@ describe('alchemy-app routes', () => {
       id: expect.any(String),
       email: expect.any(String)
     });
+  });
+
+  it('signs in a user', async () => {
+    const user = await UserService.create({
+      email: 'guy1',
+      password: '1235'
+    });
+    console.log(user);
+
+    const res = await agent.post('/api/v1/users/sessions').send({ ...user, password: '1235'  });
+    expect(res.body).toEqual({ message: 'You are Signed In' });
   });
 
 
